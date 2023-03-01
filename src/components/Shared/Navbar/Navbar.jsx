ce35b650afdf8a246452513451c08aed7e0cac30
salehAhmed19 <saleh.ahmed.mahin@gmail.com>
2023-03-02 03:44:09 +0600

    navbar added

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,3 +1,72 @@
+// import * as React from "react";
+// import PropTypes from "prop-types";
+// import AppBar from "@mui/material/AppBar";
+// import Box from "@mui/material/Box";
+// import CssBaseline from "@mui/material/CssBaseline";
+// import Divider from "@mui/material/Divider";
+// import Drawer from "@mui/material/Drawer";
+// import IconButton from "@mui/material/IconButton";
+// import List from "@mui/material/List";
+// import ListItem from "@mui/material/ListItem";
+// import ListItemButton from "@mui/material/ListItemButton";
+// import ListItemText from "@mui/material/ListItemText";
+// import MenuIcon from "@mui/icons-material/Menu";
+// import Toolbar from "@mui/material/Toolbar";
+// import Typography from "@mui/material/Typography";
+// import Button from "@mui/material/Button";
+// import "./Navbar.css"
+// import navbarData from "../../Data/NavbarData";
+
+// const drawerWidth = 240;
+// function Navbar(props) {
+//   const { window } = props;
+//   const [mobileOpen, setMobileOpen] = React.useState(false);
+
+//   const handleDrawerToggle = () => {
+//     setMobileOpen((prevState) => !prevState);
+//   };
+
+//   const drawer = (
+//     <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+//       <Typography variant="h6" sx={{ my: 2 }}>
+//       Filmmonks
+//       </Typography>
+//       <Divider />
+//       <List>
+//         {navbarData.map((item) => (
+//           <ListItem key={item} >
+//             <ListItemButton sx={{ textAlign: "center" }}>
+//               <ListItemText primary={item.route} />
+//             </ListItemButton>
+//           </ListItem>
+//         ))}
+//       </List>
+//     </Box>
+//   );
+
+//   const container =
+//     window !== undefined ? () => window().document.body : undefined;
+
+//   return (
+//    <div>
+//     <ul>
+//       <li>
+//         {navbarData.map(data=><a style={{color:"white"}} key={data._id} href={data.path}> {data.route}</a>)}
+//       </li>
+//     </ul>
+//    </div>
+//   );
+// }
+
+// Navbar.propTypes = {
+//   /**
+//    * Injected by the documentation to work in an iframe.
+//    * You won't need it on your project.
+//    */
+//   window: PropTypes.func,
+// };
+
+// export default Navbar;
 import * as React from "react";
 import PropTypes from "prop-types";
 import AppBar from "@mui/material/AppBar";
@@ -14,11 +83,19 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import "./Navbar.css"
-import navbarData from "../../Data/NavbarData";
-
+import logo from "../../../Assets/Logo/logo.png";
 
 const drawerWidth = 240;
+const navItems = [
+  "Home",
+  "About",
+  "Service",
+  "Work",
+  "Team",
+  "Monks Gallery",
+  "Contact",
+];
+
 function Navbar(props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
@@ -28,16 +105,19 @@ function Navbar(props) {
   };
 
   const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+    <Box
+      onClick={handleDrawerToggle}
+      sx={{ textAlign: "center", color: "#000" }}
+    >
       <Typography variant="h6" sx={{ my: 2 }}>
-      Filmmonks
+        MUI
       </Typography>
       <Divider />
       <List>
-        {navbarData.map((item) => (
-          <ListItem key={item} >
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
             <ListItemButton sx={{ textAlign: "center" }}>
-              <ListItemText primary={item.route} />
+              <ListItemText sx={{ color: "#000" }} primary={item} />
             </ListItemButton>
           </ListItem>
         ))}
@@ -49,13 +129,69 @@ function Navbar(props) {
     window !== undefined ? () => window().document.body : undefined;
 
   return (
-   <div>
-    <ul>
-      <li>
-        {navbarData.map(data=><a style={{color:"white"}} key={data._id} href={data.path}> {data.route}</a>)}
-      </li>
-    </ul>
-   </div>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        component="nav"
+        sx={{
+          backgroundColor: "transparent",
+          boxShadow: "none",
+          padding: "20px 150px",
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            <img className="h-7 block" src={logo} alt="" />
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <Button
+                key={item}
+                sx={{
+                  color: "#fff",
+                  "&:hover": { color: "#F45656" },
+                }}
+              >
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
   );
 }
 
