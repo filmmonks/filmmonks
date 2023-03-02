5209cd8d655a82c765a765652746f20772f54c4b
salehAhmed19 <saleh.ahmed.mahin@gmail.com>
2023-03-03 02:10:39 +0600

    footer designed

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,89 +1,72 @@
 import React from "react";
-import { Grid, Link, Typography } from "@mui/material";
-import { Container } from "@mui/system";
+import Iframe from "react-iframe";
+import { MdEmail } from "react-icons/md";
 import img from "../../../Assets/Footer/footer.png";
+import {
+  BsTelephoneFill,
+  BsFacebook,
+  BsYoutube,
+  BsInstagram,
+} from "react-icons/bs";
+
 const Footer = () => {
-  function Copyright(props) {
-    return (
-      <div className="flex justify-center items-center mt-10">
-        <p className="text-[#fff]">
-          Copyright &copy;{" "}
-          <a className="underline" href="#home">
-            Film Monks{" "}
-          </a>
-          {new Date().getFullYear()}
-        </p>
-      </div>
-    );
-  }
-  const footers = [
-    {
-      title: "Company",
-      description: ["Team", "History", "Contact us", "Locations"],
-    },
-    {
-      title: "Features",
-      description: [
-        "Cool stuff",
-        "Random feature",
-        "Team feature",
-        "Developer stuff",
-        "Another one",
-      ],
-    },
-    {
-      title: "Resources",
-      description: [
-        "Resource",
-        "Resource name",
-        "Another resource",
-        "Final resource",
-      ],
-    },
-    {
-      title: "Legal",
-      description: ["Privacy policy", "Terms of use"],
-    },
-  ];
   return (
-    <div
-      style={{ background: `url(${img})`, backgroundSize: "cover" }}
-      className=""
-    >
-      <Container
-        maxWidth="md"
-        component="footer"
-        sx={{
-          borderTop: (theme) => `1px solid ${theme.palette.divider}`,
-          py: [3, 6],
-        }}
+    <div className="mt-20">
+      <footer
+        style={{ background: `url(${img})`, backgroundSize: "cover" }}
+        className=""
       >
-        <Grid container spacing={4} justifyContent="space-evenly">
-          {footers.map((footer) => (
-            <Grid item xs={6} sm={3} key={footer.title}>
-              <Typography variant="h6" color="#fff" gutterBottom>
-                {footer.title}
-              </Typography>
-              <ul>
-                {footer.description.map((item) => (
-                  <li key={item}>
-                    <Link
-                      href="#"
-                      variant="subtitle1"
-                      color="#fff"
-                      className="cursor-pointer"
-                    >
-                      {item}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </Grid>
-          ))}
-        </Grid>
-        <Copyright sx={{ mt: 5, color: "#fff" }} />
-      </Container>
-      {/* End footer */}
+        <div className="flex flex-col lg:flex-row lg:p-28 text-[#fff]">
+          <div className="lg:pl-24">
+            <Iframe
+              url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d233667.8223943459!2d90.27919449966463!3d23.78088745514968!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka!5e0!3m2!1sen!2sbd!4v1677785620904!5m2!1sen!2sbd"
+              // width="509px"
+              // height="316px"
+              id=""
+              className="lg:w-[509px] h-[316px]"
+              display="block"
+              position="relative"
+            />
+          </div>
+          <div className="flex flex-col lg:flex-row lg:px-14">
+            <div className="mx-10">
+              <span className=" text-[#F45656] font-bold text-xl">
+                Head Quarter
+              </span>
+              <p className="link link-hover font-bold my-3">Connect Us</p>
+              <p className="link link-hover text-[#B19B9B] mb-4">
+                Office Address: House 12,
+                <br /> Block C, Future Town <br /> Housing, Mohammadpur,
+                <br /> Dhaka-1207
+              </p>
+              <p className="link link-hover text-[#B19B9B] flex items-center my-2">
+                <MdEmail className="mr-3 text-2xl text-[#fff]" />
+                jayanto.k.kundu@gmail.com
+              </p>
+              <p className="link link-hover text-[#B19B98] flex items-center">
+                <BsTelephoneFill className="mr-3 text-[#fff]" />
+                01705099380
+              </p>
+            </div>
+            <div className="mx-10 mt-8 flex flex-col lg:items-center">
+              <p className="link link-hover font-bold">Follow Us</p>
+              <p className="link link-hover text-xl my-3">
+                <BsFacebook />
+              </p>
+              <p className="link link-hover text-xl my-3">
+                <BsYoutube />
+              </p>
+              <p className="link link-hover text-xl my-3">
+                <BsInstagram />
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center bg-[#000] px-[200px] text-[#fff] py-5">
+          <p className="mr-40">Dhaka, Bangladesh </p>
+          <p>&copy; 2023 Filmmonks. All Rights Reserved </p>
+        </div>
+      </footer>
     </div>
   );
 };
