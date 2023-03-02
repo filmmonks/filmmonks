9a87d2cba9da09a02ad29d1e6f47f4ad642deb06
rashida <filmmonks001@gmail.com>
2023-03-02 22:55:36 +0600

    some chnages in navbar

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ import { createTheme } from "@mui/material/styles";
 import { green, purple } from "@mui/material/colors";
 import { CssBaseline } from "@mui/material";
 import "./index.css";
-
-import Contact from "./components/Page/Contact/Contact";
-import { Route, Routes } from "react-router-dom";
-
-
 import Footer from "./components/Shared/Footer/Footer";
 // const theme = createTheme({
 //   palette: {
@@ -63,64 +58,6 @@ function App() {
       <Footer />
     </div>
     // </ThemeProvider>
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#0F100B",
-    },
-    secondary: {
-      main: "#F45656",
-    },
-  },
-  typography: {
-    fontSize: 16,
-    fontFamily: "Inter",
-    h1: {
-      fontSize: "40px",
-      fontWeight: "400",
-    },
-    h2: {
-      fontSize: "30px",
-      fontWeight: "400",
-    },
-    h3: {
-      fontSize: "24px",
-      fontWeight: "400",
-    },
-    h4: {
-      fontSize: "20px",
-      fontWeight: "400",
-    },
-    h5: {
-      fontSize: "18px",
-      fontWeight: "400",
-    },
-    p: {
-      fontSize: "16px",
-      fontWeight: "400",
-    },
-  },
-});
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-
-      <div>
-
-      <div className="bg-[#000]">
-
-        <nav>
-          <Navbar />
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </ThemeProvider>
-
   );
 }
 
