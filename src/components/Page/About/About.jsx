7fd996ce62bff6f50633ce3a2908e363c4440fa2
Rashidaakter1 <rashidaakterchadni@gmail.com>
2023-08-06 22:25:39 +0600

    add static file in the pages

--- src/components/Page/About/About.jsx
+++ src/components/Page/About/About.jsx
@@ -1,29 +1,125 @@
+// import React from "react";
+// import "./About.css";
+// import Headline from "../../TextComponents/Headline";
+// import { Fade } from "react-reveal";
+// import styled from "styled-components";
+// import SocialMedia from "../../Shared/SocialMedia/SocialMedia"; // Import your SocialMedia component
+// import useGet from "../../../hooks/useGet";
+
+// const About = () => {
+//   const url = "https://filmmonks-server.onrender.com/api/about";
+//   const { dataSource } = useGet(url);
+//   const lastElement = dataSource.slice(-1)[0];
+
+//   const splitTextAtFirstFullStop = (text) => {
+//     const firstFullStopIndex = text?.indexOf(".");
+//     if (firstFullStopIndex === -1) {
+//       return [text];
+//     }
+//     return [
+//       text?.slice(0, firstFullStopIndex + 1),
+//       text?.slice(firstFullStopIndex + 2),
+//     ];
+//   };
+
+//   const chunks = splitTextAtFirstFullStop(lastElement?.description);
+
+//   return (
+//     <>
+//       <div
+//         id="about"
+//         className="bg-[#0F100B] xl:flex lg:block md:block block justify-between items-center lg:gap-4 md:gap-4 xl:h-screen py-4 xl:py-0 "
+//       >
+//         <Wrapper className="xl:w-[70%] lg:w-[100%] md:w-[100%] w-[100%]">
+//           <div className="xl:mx-24 lg:mx-16 mx-8 md:mx-16 my-8 text-[#FFF8F8] ">
+//             <Headline
+//               style={{ color: "white", textAlign: "left" }}
+//               content="about u"
+//               lastWord="s"
+//             />
+
+//             <Fade left>
+//               {chunks?.map((data) => (
+//                 <p className="lg:content-2 md:content-md content-sm xs:content-xs pb-6">
+//                   {data}
+//                 </p>
+//               ))}
+//             </Fade>
+//           </div>
+//         </Wrapper>
+
+//         <div className="xl:block lg:block hidden lg:w-[50%] mt-44 mx-auto">
+//           <Fade right>
+//             <div
+//               style={{ width: "70%", marginLeft: "auto", marginRight: "auto" }}
+//               className="lg:mx-24 mx-8 md:mx-16 my-8 grid md:grid-cols-2 grid-cols-2  gap-x-10 gap-y-5"
+//             >
+//               {lastElement?.imageArr?.map((item) => (
+//                 <img
+//                   style={{
+//                     width: `${item.width}`,
+//                     marginTop: `${item.marginTop}`,
+//                   }}
+//                   key={item?.title}
+//                   src={
+//                     `https://filmmonks-server.onrender.com/about/` +
+//                     item?.pathname
+//                   }
+//                   alt=""
+//                 />
+//               ))}
+//             </div>
+//           </Fade>
+//         </div>
+//         {/* mobile device */}
+//         <div className="lg:hidden block md:block">
+//           <Fade right>
+//             <div
+//               style={{ width: "70%", marginLeft: "auto", marginRight: "auto" }}
+//               className="lg:mx-24 mx-8 md:mx-16 my-8 grid grid-cols-2 gap-7"
+//             >
+//               {dataSource[0]?.imageArr?.map((item) => (
+//                 <img
+//                   key={item?.title}
+//                   src={
+//                     `https://filmmonks-server.onrender.com/about/` +
+//                     item?.pathname
+//                   }
+//                   alt=""
+//                 />
+//               ))}
+//             </div>
+//           </Fade>
+//         </div>
+
+//         {/* Add SocialMedia component */}
+//         <div
+//           style={{ width: "5%" }}
+//           className="hidden xl:flex lg:hidden md:hidden items-center justify-center pr-5"
+//         >
+//           <SocialMedia />
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// const Wrapper = styled.div``;
+
+// export default About;
+
 import React from "react";
 import "./About.css";
 import Headline from "../../TextComponents/Headline";
 import { Fade } from "react-reveal";
 import styled from "styled-components";
+import img1 from "../../../Assets/About/about1.png";
+import img2 from "../../../Assets/About/about2.jpg";
+import img3 from "../../../Assets/About/about3.jpg";
+import img4 from "../../../Assets/About/about4.png";
 import SocialMedia from "../../Shared/SocialMedia/SocialMedia"; // Import your SocialMedia component
-import useGet from "../../../hooks/useGet";
 
 const About = () => {
-  const url = "https://filmmonks-server.onrender.com/api/about";
-  const { dataSource } = useGet(url);
-  const lastElement = dataSource.slice(-1)[0];
-
-  const splitTextAtFirstFullStop = (text) => {
-    const firstFullStopIndex = text?.indexOf(".");
-    if (firstFullStopIndex === -1) {
-      return [text];
-    }
-    return [
-      text?.slice(0, firstFullStopIndex + 1),
-      text?.slice(firstFullStopIndex + 2),
-    ];
-  };
-
-  const chunks = splitTextAtFirstFullStop(lastElement?.description);
-
   return (
     <>
       <div
@@ -39,11 +135,22 @@ const About = () => {
             />
 
             <Fade left>
-              {chunks?.map((data) => (
-                <p className="lg:content-2 md:content-md content-sm xs:content-xs pb-6">
-                  {data}
-                </p>
-              ))}
+              <p className="lg:content-2 md:content-md content-sm xs:content-xs pb-6">
+                Film Monks is a production house that specializes in the
+                development and production of a wide range of visual mediums,
+                including fiction and non-fiction cinema online video content,
+                television commercials event videography and photography, music
+                videos, and promotional materials.
+              </p>
+              <p className="lg:content-2 md:content-md content-sm xs:content-xs ">
+                We place an emphasis on contemporary storylines and story
+                telling with a focus on aesthetics. We strive to create
+                cinematic experiences that entertain and provoke thought while
+                keeping in mind the emotions, arguments, traditions nature, and
+                crises that are universally accessible to our audiences. We aim
+                to produce visually stunning and impactful content for our
+                clients.
+              </p>
             </Fade>
           </div>
         </Wrapper>
@@ -54,17 +161,14 @@ const About = () => {
               style={{ width: "70%", marginLeft: "auto", marginRight: "auto" }}
               className="lg:mx-24 mx-8 md:mx-16 my-8 grid md:grid-cols-2 grid-cols-2  gap-x-10 gap-y-5"
             >
-              {lastElement?.imageArr?.map((item) => (
+              {itemData.map((item) => (
                 <img
                   style={{
                     width: `${item.width}`,
                     marginTop: `${item.marginTop}`,
                   }}
-                  key={item?.title}
-                  src={
-                    `https://filmmonks-server.onrender.com/about/` +
-                    item?.pathname
-                  }
+                  key={item.title}
+                  src={item.img}
                   alt=""
                 />
               ))}
@@ -78,13 +182,17 @@ const About = () => {
               style={{ width: "70%", marginLeft: "auto", marginRight: "auto" }}
               className="lg:mx-24 mx-8 md:mx-16 my-8 grid grid-cols-2 gap-7"
             >
-              {dataSource[0]?.imageArr?.map((item) => (
+              {itemData1.map((item) => (
                 <img
-                  key={item?.title}
-                  src={
-                    `https://filmmonks-server.onrender.com/about/` +
-                    item?.pathname
-                  }
+                  // style={{
+                  //   width: `${item.width}`,
+                  //   height: `${item.height}`,
+                  //   marginLeft: `${item.marginLeft}`,
+                  //   marginTop: `${item.marginTop}`,
+                  //   zIndex: `${item.zIndex}`,
+                  // }}
+                  key={item.title}
+                  src={item.img}
                   alt=""
                 />
               ))}
@@ -107,3 +215,53 @@ const About = () => {
 const Wrapper = styled.div``;
 
 export default About;
+
+const itemData = [
+  {
+    img: img1,
+    title: "1",
+    width: "300px",
+  },
+  {
+    img: img2,
+    title: "2",
+    width: "300px",
+  },
+  {
+    img: img3,
+    title: "1",
+    width: "300px",
+  },
+  {
+    img: img4,
+    title: "2",
+    width: "300px",
+  },
+];
+
+const itemData1 = [
+  {
+    img: img1,
+    title: "1",
+    width: "200px",
+  },
+  {
+    img: img2,
+    title: "2",
+    width: "200px",
+    marginLeft: "10px",
+  },
+  {
+    img: img3,
+    title: "1",
+    width: "200px",
+    marginTop: "10px",
+  },
+  {
+    img: img4,
+    title: "2",
+    width: "200px",
+    marginTop: "10px",
+    marginLeft: "10px",
+  },
+];
--- src/components/Page/Banner/Banner.jsx
+++ src/components/Page/Banner/Banner.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-// import banner from "../../../Assets/Banner/final hero section.png";
-// import mobileBannar from "../../../Assets/Banner/mobileBannar.png";
-// import tabBanner from "../../../Assets/Banner/hero section.png";
+import banner from "../../../Assets/Banner/final hero section.png";
+import mobileBannar from "../../../Assets/Banner/mobileBannar.png";
+import tabBanner from "../../../Assets/Banner/hero section.png";
 import SocialMedia from "../../Shared/SocialMedia/SocialMedia";
 import useGet from "../../../hooks/useGet";
 
 const Banner = () => {
-  const url = "https://filmmonks-server.onrender.com/upload";
-  const { dataSource } = useGet(url);
+  // const url = "https://filmmonks-server.onrender.com/upload";
+  // const { dataSource } = useGet(url);
 
-  const lastElement = dataSource.slice(-1)[0];
+  // const lastElement = dataSource.slice(-1)[0];
 
-  const pathnames = lastElement?.path?.map(({ pathname }) => pathname);
+  // const pathnames = lastElement?.path?.map(({ pathname }) => pathname);
   return (
     <>
       <div className="relative">
-        {pathnames?.slice(2, 3)?.map((data) => (
+        {/* {pathnames?.slice(2, 3)?.map((data) => (
           <img
             className=" h-screen w-[100%] hidden lg:block "
             src={`https://filmmonks-server.onrender.com/home/` + data}
@@ -36,7 +36,22 @@ const Banner = () => {
             src={`https://filmmonks-server.onrender.com/home/` + data}
             alt="banner"
           />
-        ))}
+        ))} */}
+        <img
+          className=" h-screen w-[100%] hidden lg:block "
+          src={banner}
+          alt="banner"
+        />
+        <img
+          className=" h-screen w-[100%] hidden md:block lg:hidden "
+          src={tabBanner}
+          alt="banner"
+        />
+        <img
+          className=" h-[60vh] w-[100%] lg:hidden md:hidden block"
+          src={mobileBannar}
+          alt="banner"
+        />
       </div>
 
       <div
@@ -52,4 +67,3 @@ const Banner = () => {
   );
 };
 export default Banner;
-
